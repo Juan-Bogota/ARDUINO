621a57069c8e87dfaf9ccb2c716d23e8f288cdf8
Juan-Bogota <1213@holbertonschool.com>
2020-02-05 07:48:07 +0000

    [feature] add file luces example 3

--- luces_3/luces_3.ino
+++ luces_3/luces_3.ino
@@ -1,41 +1,40 @@
+//Function that turns leds on and off randomly with 200 ms delay
 
-int sensorPin = 0;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int pot = 0;  // variable to store the value coming from the sensor
+int num;
+int i;
+int numAnt = 0;
 
-void setup() {
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(11,OUTPUT);
+
+void setup()
+{
+	for( i = 4; i < 12; i++)
+	{
+		pinMode(i, OUTPUT);
+		digitalWrite(i, LOW);
+	}
 }
 
-void loop() {
-  // read the value from the sensor:
-  pot = analogRead(sensorPin);
-  // turn the ledPin on
-  if (pot >= 0 && pot < 350)
-  {
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(12,LOW);
-  digitalWrite(11,LOW);
-  // stop the program for <sensorValue> milliseconds:
-  delay(300);
-  }
-  else if (pot >= 350 && pot < 750)  
-  // turn the ledPin off:
-  {
-      digitalWrite(ledPin, LOW);
-      digitalWrite(12,HIGH);
-      digitalWrite(11,LOW);
-      delay(300);
-  }
-  else
-    {
-    digitalWrite(ledPin, LOW);
-    digitalWrite(12,LOW);
-  digitalWrite(11,HIGH);
-    // stop the program for for <sensorValue> milliseconds:
-  delay(300);
-  }
- }
+void loop()
+{
+	for( i = 0; i < 8; ++i)
+	{
+		num = random(4,12);
+		if (num != numAnt)
+		{
+			digitalWrite(num,HIGH);
+			delay(200);
+  		}
+		numAnt = num;
+	}
+
+	for( i = 0; i < 8; i++)
+	{
+		num = random(4,12);
+		if (num != numAnt)
+		{
+			digitalWrite(num,LOW);
+			delay(200);
+  		}
+   		numAnt = num;
+	}
+}
